cc8a6dc376f7db3463c00a94ff217d58e733fff8
gabreel05 <gabrielgoncalvesmachado@hotmail.com>
2020-06-05 04:45:45 -0300

    Initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, Image, ImageBackground, StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from "@react-navigation/native";
+import PickerSelect from 'react-native-picker-select';
+import { Picker } from "@react-native-community/picker";
+import axios from 'axios';
+
+interface IBGEResponseState {
+  sigla: string;
+}
+
+interface IBGEResponseCiy {
+  nome: string;
+}
+
+const Home: React.FC = () => {
+  const navigation = useNavigation();
+
+  const [selectedState, setSelectedState] = useState<string>('0')
+  const [selectedCity, setSelectedCity] = useState<string>('0')
+
+  const [states, setStates] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  useEffect(() => {
+    axios.get<IBGEResponseState[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+      const stateInitials = response.data.map(state => state.sigla);
+
+      setStates(stateInitials);
+    })
+  }, []);
+
+  useEffect(() => {
+    if (selectedState === '0') return;
+
+    axios.get<IBGEResponseCiy[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/PR/municipios').then(response => {
+      const cityNames = response.data.map(city => city.nome);
+
+      setCities(cityNames);
+    })
+  }, []);
+
+  function handleNavigatePoints() {
+    navigation.navigate('Points', {
+      selectedState,
+      selectedCity
+    });
+  }
+
+  function handleSelectState(value: React.ReactText) {
+    const state = value.toString()
+
+    setSelectedState(state);
+    
+    console.log(state);
+  }
+
+  function handleSelectedCity(value: React.ReactText) {
+    const city = value.toString()
+
+    setSelectedCity(city);
+  }
+
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ImageBackground 
+        source={require('../../assets/home-background.png')} 
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')}/>
+          <View>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={handleNavigatePoints}>
+            <View style={styles.buttonIcon}>
+                <Icon name="arrow-right" color="#fff" size={24} />
+            </View>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+
+});
+
+export default Home;
